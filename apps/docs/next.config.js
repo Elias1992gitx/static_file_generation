ccaba73cf71c0d2c172ef80a31e0157ee5eb46c4
Elias Chane <eliashailu492@gmail.com>
2024-12-02 15:31:01 +0300

    static file generation from nextjs app

---
+++ apps/docs/next.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+      unoptimized: true
+    }
+  };
+  
+  export default nextConfig;
---
+++ apps/web/next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+      unoptimized: true
+    }
+  };
+  
+  export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
